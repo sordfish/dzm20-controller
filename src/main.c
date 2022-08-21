518f7791cbc6f34d9490a0a55cff7959cd20f7bd
sordfish <sordfish2050@gmail.com>
2022-08-21 08:37:03 +0100

    move to nucei sdk

--- src/main.c
+++ src/main.c
@@ -1,82 +1,102 @@
-/*!
-    \file  main.c
-    \brief USART printf
-
-    \version 2019-6-5, V1.0.0, firmware for GD32VF103
-*/
-
-/*
-    Copyright (c) 2019, GigaDevice Semiconductor Inc.
-
-    Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice, this 
-       list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice, 
-       this list of conditions and the following disclaimer in the documentation 
-       and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holder nor the names of its contributors 
-       may be used to endorse or promote products derived from this software without 
-       specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
-OF SUCH DAMAGE.
-*/
-
-#include "gd32vf103.h"
+// See LICENSE for license details.
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "nuclei_sdk_soc.h"
 
-/*!
-    \brief      main function
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
-int main(void)
+void print_misa(void)
 {
-    /* enable GPIO clock */
-    rcu_periph_clock_enable(RCU_GPIOA);
-
-    /* enable USART clock */
-    rcu_periph_clock_enable(RCU_USART0);
-
-    /* connect port to USARTx_Tx */
-    gpio_init(GPIOA, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_9);
-
-    /* connect port to USARTx_Rx */
-    gpio_init(GPIOA, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_50MHZ, GPIO_PIN_10);
-
-    /* USART configure */
-    usart_deinit(USART0);
-    usart_baudrate_set(USART0, 115200U);
-    usart_word_length_set(USART0, USART_WL_8BIT);
-    usart_stop_bit_set(USART0, USART_STB_1BIT);
-    usart_parity_config(USART0, USART_PM_NONE);
-    usart_hardware_flow_rts_config(USART0, USART_RTS_DISABLE);
-    usart_hardware_flow_cts_config(USART0, USART_CTS_DISABLE);
-    usart_receive_config(USART0, USART_RECEIVE_ENABLE);
-    usart_transmit_config(USART0, USART_TRANSMIT_ENABLE);
-    usart_enable(USART0);
-
-    printf("a usart transmit test example!\n");
-    while(1);
-}
-
-/* retarget the C library printf function to the USART */
-int _put_char(int ch)
-{
-    usart_data_transmit(USART0, (uint8_t) ch );
-    while ( usart_flag_get(USART0, USART_FLAG_TBE)== RESET){
+    CSR_MISA_Type misa_bits = (CSR_MISA_Type) __RV_CSR_READ(CSR_MISA);
+    static char misa_chars[30];
+    uint8_t index = 0;
+    if (misa_bits.b.mxl == 1) {
+        misa_chars[index++] = '3';
+        misa_chars[index++] = '2';
+    } else if (misa_bits.b.mxl == 2) {
+        misa_chars[index++] = '6';
+        misa_chars[index++] = '4';
+    } else if (misa_bits.b.mxl == 3) {
+        misa_chars[index++] = '1';
+        misa_chars[index++] = '2';
+        misa_chars[index++] = '8';
+    }
+    if (misa_bits.b.i) {
+        misa_chars[index++] = 'I';
+    }
+    if (misa_bits.b.m) {
+        misa_chars[index++] = 'M';
+    }
+    if (misa_bits.b.a) {
+        misa_chars[index++] = 'A';
+    }
+    if (misa_bits.b.b) {
+        misa_chars[index++] = 'B';
+    }
+    if (misa_bits.b.c) {
+        misa_chars[index++] = 'C';
+    }
+    if (misa_bits.b.e) {
+        misa_chars[index++] = 'E';
+    }
+    if (misa_bits.b.f) {
+        misa_chars[index++] = 'F';
+    }
+    if (misa_bits.b.d) {
+        misa_chars[index++] = 'D';
+    }
+    if (misa_bits.b.q) {
+        misa_chars[index++] = 'Q';
+    }
+    if (misa_bits.b.h) {
+        misa_chars[index++] = 'H';
+    }
+    if (misa_bits.b.j) {
+        misa_chars[index++] = 'J';
+    }
+    if (misa_bits.b.l) {
+        misa_chars[index++] = 'L';
+    }
+    if (misa_bits.b.n) {
+        misa_chars[index++] = 'N';
+    }
+    if (misa_bits.b.s) {
+        misa_chars[index++] = 'S';
+    }
+    if (misa_bits.b.p) {
+        misa_chars[index++] = 'P';
+    }
+    if (misa_bits.b.t) {
+        misa_chars[index++] = 'T';
+    }
+    if (misa_bits.b.u) {
+        misa_chars[index++] = 'U';
+    }
+    if (misa_bits.b.v) {
+        misa_chars[index++] = 'V';
+    }
+    if (misa_bits.b.x) {
+        misa_chars[index++] = 'X';
     }
 
-    return ch;
+    misa_chars[index++] = '\0';
+
+    printf("MISA: RV%s\r\n", misa_chars);
 }
+
+int main(void)
+{
+    srand(__get_rv_cycle()  | __get_rv_instret() | __RV_CSR_READ(CSR_MCYCLE));
+    uint32_t rval = rand();
+    rv_csr_t misa = __RV_CSR_READ(CSR_MISA);
+
+    printf("MISA: 0x%lx\r\n", misa);
+    print_misa();
+
+    for (int i = 0; i < 20; i ++) {
+        printf("%d: Hello World From Nuclei RISC-V Processor!\r\n", i);
+    }
+
+    return 0;
+}
+
+
